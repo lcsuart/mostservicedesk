aaa909133e29b38fb8bf5f89eba074ef918cd977
lcsuart <lcsuart@gmail.com>
2026-09-04 18:34:04 -0300

    fix: show department add action for config managers

--- front/department.php
+++ front/department.php
@@ -6,7 +6,7 @@ Session::checkRight('config', READ);
 Html::header('MOST Service Desk', $_SERVER['PHP_SELF'], 'config', 'plugins');
 
 echo '<div class="center"><h2>Departamentos de tickets</h2>';
-if (Session::haveRight('config', CREATE)) {
+if (Session::haveRight('config', UPDATE)) {
     echo '<p><a class="btn btn-primary" href="department.form.php">Adicionar departamento</a></p>';
 }
 echo '</div>';
