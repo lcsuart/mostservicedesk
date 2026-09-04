2b54b7f5f19e0f8ca98db02d0adc2aaee56699cd
lcsuart <lcsuart@gmail.com>
2026-09-04 18:06:57 -0300

    feat: add front/department.php

---
+++ front/department.php
@@ -0,0 +1,15 @@
+<?php
+
+use GlpiPlugin\Mostservicedesk\Department;
+
+Session::checkRight('config', READ);
+Html::header('MOST Service Desk', $_SERVER['PHP_SELF'], 'config', 'plugins');
+
+echo '<div class="center"><h2>Departamentos de tickets</h2>';
+if (Session::haveRight('config', CREATE)) {
+    echo '<p><a class="btn btn-primary" href="department.form.php">Adicionar departamento</a></p>';
+}
+echo '</div>';
+
+Search::show(Department::class);
+Html::footer();
