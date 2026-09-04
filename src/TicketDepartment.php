37f6e838e703eae89522959556e11f10de9a4a0b
lcsuart <lcsuart@gmail.com>
2026-09-04 18:51:26 -0300

    fix: map TicketDepartment to plugin table

--- src/TicketDepartment.php
+++ src/TicketDepartment.php
@@ -7,4 +7,9 @@ use CommonDBTM;
 class TicketDepartment extends CommonDBTM
 {
     public static $rightname = 'ticket';
+
+    public static function getTable($classname = null): string
+    {
+        return 'glpi_plugin_mostservicedesk_tickets';
+    }
 }
